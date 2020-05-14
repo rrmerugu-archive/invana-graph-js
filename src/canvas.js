c7fc075f122ea0865cf700524497f237e677b7f4
rrmerugu <rrmerugu@gmail.com>
2020-05-14 11:51:01 +0530

    code cleanups

--- src/canvas.js
+++ src/canvas.js
@@ -3,6 +3,7 @@ const nodesG = svg.select("g.nodes")
 const linksG = svg.select("g.links")
 const nodeRadius = 12;
 const linkDistance = 300;
+const linkCurvature = .55;
 
 var graphs = {
     "nodes": [{
@@ -80,39 +81,6 @@ var graphs = {
 }
 
 
-// // get the
-// let graphLinksCountAggregate = {};
-// graphs.links.forEach(function (d, i) {
-//     const uniqueId = d.target + "-" + d.source;
-//     if (uniqueId in graphLinksCountAggregate) {
-//         graphLinksCountAggregate[uniqueId] += 1;
-//     } else {
-//         graphLinksCountAggregate[uniqueId] = 1;
-//     }
-// });
-//
-// graphs.links.forEach(function (d, i) {
-//     let _ = graphLinksCountAggregate[d.target + "-" + d.source];
-//
-//     if (d.source + "-" + d.target in graphLinksCountAggregate) {
-//         _ += graphLinksCountAggregate[d.source + "-" + d.target]
-//     }
-//     graphs.links[i].nLinksbetweenNodes = _
-// });
-
-
-// graphs.links.forEach(function (s, i) {
-//     graphs.links[i].sameIndex = (i + 1);
-//     graphs.links[i].sameTotal = graphs.links.length;
-//     graphs.links[i].sameTotalHalf = (graphs.links[i].sameTotal / 2);
-//     graphs.links[i].sameUneven = ((graphs.links[i].sameTotal % 2) !== 0);
-//     graphs.links[i].sameMiddleLink = ((graphs.links[i].sameUneven === true) && (Math.ceil(graphs.links[i].sameTotalHalf) === graphs.links[i].sameIndex));
-//     graphs.links[i].sameLowerHalf = (graphs.links[i].sameIndex <= graphs.links[i].sameTotalHalf);
-//     graphs.links[i].sameArcDirection = graphs.links[i].sameLowerHalf ? 0 : 1;
-//     graphs.links[i].sameIndexCorrected = graphs.links[i].sameLowerHalf ? graphs.links[i].sameIndex : (graphs.links[i].sameIndex - Math.ceil(graphs.links[i].sameTotalHalf));
-//
-//
-// });
 graphs.links.forEach(function (link) {
 
     // find other links with same target+source or source+target
@@ -127,16 +95,6 @@ graphs.links.forEach(function (link) {
 
     let sameAll = same.concat(sameAlt);
     sameAll.forEach(function (s, i) {
-        //
-        // s.sameIndex = (i + 1);
-        // s.sameTotal = sameAll.length;
-        // s.sameTotalHalf = (s.sameTotal / 2);
-        // s.sameUneven = ((s.sameTotal % 2) !== 0);
-        // s.sameMiddleLink = ((s.sameUneven === true) && (Math.ceil(s.sameTotalHalf) === s.sameIndex));
-        // s.sameLowerHalf = (s.sameIndex <= s.sameTotalHalf);
-        // s.sameArcDirection = s.sameLowerHalf ? 0 : 1;
-        // s.sameIndexCorrected = s.sameLowerHalf ? s.sameIndex : (s.sameIndex - Math.ceil(s.sameTotalHalf));
-        //
 
         s.sameIndex = (i + 1);
         s.sameTotal = sameAll.length;
@@ -147,19 +105,9 @@ graphs.links.forEach(function (link) {
         s.sameArcDirection = s.sameLowerHalf ? 0 : 1;
         s.sameIndexCorrected = s.sameLowerHalf ? s.sameIndex : (s.sameIndex - Math.ceil(s.sameTotalHalf));
 
-
         if (s.sameIndexCorrected === 2) {
             s.sameArcDirection = 1;
         }
-
-        // s.sameIndex = (i + 1);
-        // s.sameTotal = sameAll.length;
-        // s.sameTotalHalf = (s.sameTotal / 2);
-        // s.sameUneven = ((s.sameTotal % 2) !== 0);
-        // s.sameMiddleLink = ((s.sameUneven === true) && (Math.ceil(s.sameTotalHalf) === s.sameIndex));
-        // s.sameLowerHalf = (s.sameIndex <= s.sameTotalHalf);
-        // s.sameArcDirection = s.sameLowerHalf ? 0 : 1;
-        // s.sameIndexCorrected = s.sameLowerHalf ? s.sameIndex : (s.sameIndex - Math.ceil(s.sameTotalHalf));
     });
 });
 
@@ -176,12 +124,6 @@ graphs.links.forEach(function (link, i) {
     graphs.links[i].maxSameHalf = Math.round(maxSame / 3);
 });
 
-// // sort by  sameIndex
-// graphs.links.sort(function (a, b) {
-//     if (a.sameIndex < b.sameIndex) return -1;
-//     if (a.sameIndex > b.sameIndex) return 1;
-//     return 0;
-// });
 
 svg.append("svg:defs").selectAll("marker")
     .data(graphs.links)
@@ -258,9 +200,9 @@ const nodes = nodesG
     .attr("cursor", "pointer")
     .attr("class", "node")
     .call(d3.drag()
-        .on("start", dragstarted)
+        .on("start", dragStarted)
         .on("drag", dragged)
-        .on("end", dragended));
+        .on("end", dragEnded));
 
 const nodeBgColor = "#333333";
 const nodeBorderColor = "#e48a8a";
@@ -304,142 +246,27 @@ simulation.force("link", d3.forceLink().links(linksData)
     .id((d, i) => d.id)
     .distance(linkDistance));
 
-// function linkArc(d) {
-//
-//     //
-//     var dx = (d.target.x - d.source.x),
-//         dy = (d.target.y - d.source.y),
-//         dr = Math.sqrt(dx * dx + dy * dy);
-//     // unevenCorrection = (d.sameUneven ? 0 : 0.5);
-//     // // let unevenCorrection = (d.sameTotal % d.sameIndexCorrected === 1) ? 0 : 0.5;
-//     // // curvature term defines how tight the arcs are (lower number = tigher curve)
-//     // var curvature = 1.8,
-//     //     arc = (1.0 / curvature) * ((dr * d.maxSameHalf) / (d.sameIndexCorrected ));
-//     //
-//     // //console.log(d.maxSameHalf)
-//     // //d.maxSameHalf always showing zero...
-//     // if (d.sameMiddleLink) {
-//     //     arc = 0;
-//     // }
-//
-//     // let unevenCorrection = (d.sameUneven === 0 ? 0 : 0.5);
-//     let unevenCorrection = (d.sameTotal % d.sameIndexCorrected === 1) ? 0 : 0.5;
-//     // curvature term defines how tight the arcs are (lower number = tigher curve)
-//
-//     // console.log("dr", dr)
-//     // console.log("d", d)
-//     // console.log("unevenCorrection", unevenCorrection)
-//
-//     var curvature = 2.5,
-//         arc = (1.0 / curvature) * ((dr * d.maxSameHalf) / (d.sameIndexCorrected - unevenCorrection));
-//
-//     // console.log("======arc", arc)
-//     if (d.sameMiddleLink) {
-//         arc = 0;
-//     }
-//     // console.log("=============")
-//
-//     return "M" + d.source.x + "," + d.source.y + "A" + arc + "," + arc + " 0 0," + d.sameArcDirection + " " + d.target.x + "," + d.target.y;
-// }
-// some more info: http://stackoverflow.com/questions/11368339/drawing-multiple-edges-between-two-nodes-with-d3
+
 function linkArc(d) {
-    var dx = (d.target.x - d.source.x),
+    let dx = (d.target.x - d.source.x),
         dy = (d.target.y - d.source.y),
         dr = Math.sqrt(dx * dx + dy * dy),
-        unevenCorrection = (d.sameUneven ? 0 : 0.5);
-
-    var curvature = 0.55;
-    arc = ((dr * d.maxSameHalf) / (d.sameIndexCorrected - unevenCorrection)) * curvature;
-    // arc = ((dr * d.maxSameHalf) / (d.sameIndex - unevenCorrection)) * curvature;
-
-
-    // var curvature = 2.04,
-    //     arc = (1.0 / curvature) * ((dr * d.maxSameHalf) / (d.sameIndexCorrected - unevenCorrection));
-
-    if (d.sameMiddleLink
-        // && (d.sameTotal % d.sameIndexCorrected === 1)
-    ) {
+        unevenCorrection = (d.sameUneven ? 0 : 0.5),
+        arc = ((dr * d.maxSameHalf) / (d.sameIndexCorrected - unevenCorrection)) * linkCurvature;
+    if (d.sameMiddleLink) {
         arc = 0;
     }
-
-
     return "M" + d.source.x + "," + d.source.y + "A" + arc + "," + arc + " 0 0," + d.sameArcDirection + " " + d.target.x + "," + d.target.y;
 }
 
 function ticked() {
     linkPaths.attr("d", linkArc)
-
-    // linkPaths.attr("d", function (d) {
-    //     // length of current path
-    //     var pl = this.getTotalLength(),
-    //         // radius of circle plus backoff
-    //         r = (nodeRadius) * 3,
-    //         // position close to where path intercepts circle
-    //         m = this.getPointAtLength(pl - r);
-    //
-    //     // var dx = m.x - d.source.x,
-    //     //     dy = m.y - d.source.y,
-    //     //     dr = Math.sqrt(dx * dx + dy * dy);
-    //
-    //     // curvature term defines how tight the arcs are (lower number = tigher curve)
-    //     var curvature = 1.5;
-    //
-    //
-    //     // graphLinksCountAggregate[uniqueId] = 1
-    //     // graphs.links[i].nLinksbetweenNodes = graphLinksCountAggregate[uniqueId];
-    //     //
-    //     // var unevenCorrection = (d.nLinksbetweenNodes % d.sameIndexCorrected === 1 ? 0 : 0.5);
-    //     //
-    //     // let arc;
-    //     // //d.maxSameHalf always showing zero...
-    //     // if ((d.nLinksbetweenNodes === 1 && d.sameIndexCorrected === 1)
-    //     //     || (d.nLinksbetweenNodes === 3 && d.sameIndexCorrected === 1)
-    //     //     || (d.nLinksbetweenNodes === 5 && d.sameIndexCorrected === 1)
-    //     // ) {
-    //     // //        if (d.sameMiddleLink === 0) {
-    //     //
-    //     //
-    //     //
-    //     //     arc = 0;
-    //     // } else {
-    //     //     console.log("====, ", unevenCorrection)
-    //     //     arc = (1.0 / curvature) * ((dr * d.maxSameHalf) / (d.sameIndexCorrected - unevenCorrection));
-    //     // }
-    //
-    //           var dx = (d.target.x - d.source.x),
-    //             dy = (d.target.y - d.source.y),
-    //             dr = Math.sqrt(dx * dx + dy * dy),
-    //             unevenCorrection = (d.sameUneven ? 0 : 0.5);
-    //         // curvature term defines how tight the arcs are (lower number = tigher curve)
-    //         var curvature = 1.8;
-    //
-    //         console.log("==unevenCorrection", unevenCorrection)
-    //         let arc = (1.0 / curvature) * ((dr * d.maxSameHalf) );// / (d.sameIndexCorrected - unevenCorrection));
-    //
-    //         //console.log(d.maxSameHalf)
-    //         //d.maxSameHalf always showing zero...
-    //         if (d.sameMiddleLink) {
-    //             arc = 0;
-    //         }
-    //
-    //
-    //     // console.log("assoc", document.querySelectorAll('[association-id*="link-' + d.target.id + "-" + d.source.id + '"]').length)
-    //     // if (document.querySelectorAll('[association-id*="link-' + d.target.id + "-" + d.source.id + '"]').length === 1) {
-    //     //     arc = 0;
-    //     // }
-    //
-    //     return "M" + d.source.x + "," + d.source.y + "A" + arc + "," + arc + " 0 0," + d.sameArcDirection + " " + d.target.x + "," + d.target.y;
-    // });
-
-
     nodes
         .attr("transform", d => `translate(${d.x}, ${d.y})`);
-
-
 }
 
-function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+function dragStarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.8).restart();
     d.fx = d.x;
     d.fy = d.y;
 }
@@ -449,8 +276,8 @@ function dragged(d) {
     d.fy = d3.event.y;
 }
 
-function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
+function dragEnded(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.8);
     d.fx = null;
     d.fy = null;
 }
